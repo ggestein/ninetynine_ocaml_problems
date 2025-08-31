b85674ae3eb5878c691d5b98cb4c9016cc75738f
ggestein <ggestein@outlook.com>
2025-08-31 20:19:03 +0800

    add compress and pack solution

--- lists.ml
+++ lists.ml
@@ -57,3 +57,36 @@ let rec flatten = function
     )
     | x :: rest -> (flatten [x]) @ (flatten rest)
 ;;
+
+(*
+  Eliminate Duplicates
+  Eliminate consecutive duplicates of list elements.
+
+  # compress ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+  - : string list = ["a"; "b"; "c"; "a"; "d"; "e"]
+ *)
+let rec compress = function
+  | [] -> []
+  | [x] -> [x]
+  | x :: y :: rest ->
+     let yrest = y :: rest in
+     if x = y then (compress yrest) else (x :: (compress yrest))
+;;
+(*
+  Pack Consecutive Duplicates
+  Pack consecutive duplicates of list elements into sublists.
+
+  # pack ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e"];;
+  - : string list list =
+  [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+  ["e"; "e"; "e"; "e"]]
+ *)
+let rec pack = function
+  | [] -> []
+  | [x] -> [[x]]
+  | x :: rest ->
+     let pr = pack rest in
+     match pr with
+     | [] -> [[x]]
+     | y :: rest -> if (List.hd y) = x then (x :: y) :: rest else [x] :: y :: rest
+;;
