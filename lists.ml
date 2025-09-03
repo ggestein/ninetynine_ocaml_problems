5b5f9a998ea1f7682d1e51357b7d57a2c232dfae
ggestein <ggestein@outlook.com>
2025-09-03 22:38:56 +0800

    re-format with emacs TAB

--- lists.ml
+++ lists.ml
@@ -1,118 +1,118 @@
 (*
-    Tail of a List
-    Write a function last : 'a list -> 'a option that returns the last element of a list
+  Tail of a List
+  Write a function last : 'a list -> 'a option that returns the last element of a list
 
-    # last ["a" ; "b" ; "c" ; "d"];;
-    - : string option = Some "d"
-    # last [];;
-    - : 'a option = None
-*)
+  # last ["a" ; "b" ; "c" ; "d"];;
+  - : string option = Some "d"
+  # last [];;
+  - : 'a option = None
+ *)
 let rec last = function
-    | [] -> None
-    | [x] -> Some x
-    | x :: rest -> last rest
+  | [] -> None
+  | [x] -> Some x
+  | x :: rest -> last rest
 ;;
 
 (*
-    Last Two Elements of a List
-    Find the last two (last and penultimate) elements of a list.
+  Last Two Elements of a List
+  Find the last two (last and penultimate) elements of a list.
 
-    # last_two ["a"; "b"; "c"; "d"];;
-    - : (string * string) option = Some ("c", "d")
-    # last_two ["a"];;
-    - : (string * string) option = None
-*)
+  # last_two ["a"; "b"; "c"; "d"];;
+  - : (string * string) option = Some ("c", "d")
+  # last_two ["a"];;
+  - : (string * string) option = None
+ *)
 let rec last_two = function
-    | [] -> None
-    | [x] -> None
-    | [x; y] -> Some (x, y)
-    | x :: rest -> last_two rest
+  | [] -> None
+  | [x] -> None
+  | [x; y] -> Some (x, y)
+  | x :: rest -> last_two rest
 ;;
 
 (*
-    N'th element of a List
-    Find the N'th element of a list.
+  N'th element of a List
+  Find the N'th element of a list.
 
-    # at 2 ["a"; "b"; "c"; "d"; "e"];;
-    - : string option = Some "c"
-    # at 2 ["a"];;
-    - : string option = None
-*)
+  # at 2 ["a"; "b"; "c"; "d"; "e"];;
+  - : string option = Some "c"
+  # at 2 ["a"];;
+  - : string option = None
+ *)
 let rec at n (xs: 'a list) =
-    if n < 0 then None
-    else if n > 0 then
-        match xs with
-        | [] -> None
-        | x :: rest -> at (n - 1) rest
-    else match xs with
-    | [] -> None
-    | x :: _ -> Some x
+  if n < 0 then None
+  else if n > 0 then
+      match xs with
+      | [] -> None
+      | x :: rest -> at (n - 1) rest
+  else match xs with
+       | [] -> None
+       | x :: _ -> Some x
 ;;
 
 (*
-    Length of a List
-    Find the number of elements of a list.
+  Length of a List
+  Find the number of elements of a list.
 
-    # length ["a"; "b"; "c"];;
-    - : int = 3
-    # length [];;
-    - : int = 0
-*)
+  # length ["a"; "b"; "c"];;
+  - : int = 3
+  # length [];;
+  - : int = 0
+ *)
 let rec length = function
-    | [] -> 0
-    | x :: rest -> 1 + (length rest)
+  | [] -> 0
+  | x :: rest -> 1 + (length rest)
 ;;
 
 (*
-    Reverse a List
-    Reverse a list.
-    OCaml standard library has List.rev but we ask that you reimplement it.
+  Reverse a List
+  Reverse a list.
+  OCaml standard library has List.rev but we ask that you reimplement it.
 
-    # rev ["a"; "b"; "c"];;
-    - : string list = ["c"; "b"; "a"]
-*)
+  # rev ["a"; "b"; "c"];;
+  - : string list = ["c"; "b"; "a"]
+ *)
 let rec rev = function
-    | [] -> []
-    | x :: rest -> (rev rest) @ [x]
+  | [] -> []
+  | x :: rest -> (rev rest) @ [x]
 ;;
 
 (*
-    Palindrome
-    Find out whether a list is a palindrome.
-    Hint: A palindrome is its own reverse.
+  Palindrome
+  Find out whether a list is a palindrome.
+  Hint: A palindrome is its own reverse.
 
-    # is_palindrome ["x"; "a"; "m"; "a"; "x"];;
-    - : bool = true
-    # not (is_palindrome ["a"; "b"]);;
-    - : bool = true
-*)
+  # is_palindrome ["x"; "a"; "m"; "a"; "x"];;
+  - : bool = true
+  # not (is_palindrome ["a"; "b"]);;
+  - : bool = true
+ *)
 let rec is_palindrome (xs: 'a list) =
-    xs = (List.rev xs)
+  xs = (List.rev xs)
 ;;
 
 (*
-    Flatten a List
-    Flatten a nested list structure.
+  Flatten a List
+  Flatten a nested list structure.
 
-    type 'a node =
-    | One of 'a 
-    | Many of 'a node list
-    # flatten [One "a"; Many [One "b"; Many [One "c" ;One "d"]; One "e"]];;
-    - : string list = ["a"; "b"; "c"; "d"; "e"]
-*)
+  type 'a node =
+  | One of 'a 
+  | Many of 'a node list
+  # flatten [One "a"; Many [One "b"; Many [One "c" ;One "d"]; One "e"]];;
+  - : string list = ["a"; "b"; "c"; "d"; "e"]
+ *)
 type 'a node =
-    | One of 'a
-    | Many of 'a node list
+  | One of 'a
+  | Many of 'a node list
 ;;
 let rec flatten = function
-    | [] -> []
-    | [x] ->
-    (
-        match x with
-        | One i -> [i]
-        | Many il -> flatten il 
-    )
-    | x :: rest -> (flatten [x]) @ (flatten rest)
+  | [] -> []
+  | [x] ->
+     (
+         match x with
+         | One i -> [i]
+         | Many il -> flatten il 
+     )
+  | x :: rest -> (flatten [x]) @ (flatten rest)
 ;;
 
 (*
@@ -150,126 +150,126 @@ let rec pack = function
 ;;
 
 (*
-    Run-Length Encoding
-    If you need so, refresh your memory about run-length encoding.
-    [http://en.wikipedia.org/wiki/Run-length_encoding]
+  Run-Length Encoding
+  If you need so, refresh your memory about run-length encoding.
+  [http://en.wikipedia.org/wiki/Run-length_encoding]
 
-    # encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
-    - : (int * string) list =
-    [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
-*)
+  # encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+  - : (int * string) list =
+  [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
+ *)
 let encode_0 xs =
-    let inner_fold_func acc x =
-        match acc with
-        | [] -> [(1, x)]
-        | a :: rest ->
-            let (cnt, letter) = a in
-            if letter = x then (cnt + 1, letter) :: rest
-            else (1, x) :: a :: rest in
-    List.rev (List.fold_left inner_fold_func [] xs)
+  let inner_fold_func acc x =
+    match acc with
+    | [] -> [(1, x)]
+    | a :: rest ->
+       let (cnt, letter) = a in
+       if letter = x then (cnt + 1, letter) :: rest
+       else (1, x) :: a :: rest in
+  List.rev (List.fold_left inner_fold_func [] xs)
 ;;
 (*
-    Modified Run-Length Encoding
-    Modify the result of the previous problem in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
-    Since OCaml lists are homogeneous, one needs to define a type to hold both single elements and sub-lists.
+  Modified Run-Length Encoding
+  Modify the result of the previous problem in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+  Since OCaml lists are homogeneous, one needs to define a type to hold both single elements and sub-lists.
 
-    type 'a rle =
-    | One of 'a
-    | Many of int * 'a
-    # encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
-    - : string rle list =
-    [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
-    Many (4, "e")]
-*)
+  type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+  # encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+  - : string rle list =
+  [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+  Many (4, "e")]
+ *)
 type 'a rle =
-| One of 'a
-| Many of int * 'a
+  | One of 'a
+  | Many of int * 'a
 ;;
 let encode_1 xs =
-    let mff x =
-        let (cnt, letter) = x in
-        if cnt = 1 then One letter else Many (cnt, letter) in
-    let mfb x =
-        match x with
-        | One letter -> (1, letter)
-        | Many (cnt, letter) -> (cnt, letter) in
-    let encode_1_fold acc x =
-        match acc with
-        | [] -> [One x]
-        | a :: rest ->
-            let (cnt, letter) = (mfb a) in
-            if letter = x then mff ((cnt + 1), letter) :: rest
-            else mff (1, x) :: a :: rest in
-    List.rev (List.fold_left encode_1_fold [] xs)
+  let mff x =
+    let (cnt, letter) = x in
+    if cnt = 1 then One letter else Many (cnt, letter) in
+  let mfb x =
+    match x with
+    | One letter -> (1, letter)
+    | Many (cnt, letter) -> (cnt, letter) in
+  let encode_1_fold acc x =
+    match acc with
+    | [] -> [One x]
+    | a :: rest ->
+       let (cnt, letter) = (mfb a) in
+       if letter = x then mff ((cnt + 1), letter) :: rest
+       else mff (1, x) :: a :: rest in
+  List.rev (List.fold_left encode_1_fold [] xs)
 ;;
 
 (*
-    Decode a Run-Length Encoded List
-    Given a run-length code list generated as specified in the previous problem, construct its uncompressed version.
+  Decode a Run-Length Encoded List
+  Given a run-length code list generated as specified in the previous problem, construct its uncompressed version.
 
-    #  decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")];;
-    - : string list =
-    ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]
-*)
+  #  decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")];;
+  - : string list =
+  ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]
+ *)
 let rec decode xs =
-    let rec dup n e =
-        if n = 0 then []
-        else e :: (dup (n - 1) e) in
-    let dup_expand = function
+  let rec dup n e =
+    if n = 0 then []
+    else e :: (dup (n - 1) e) in
+  let dup_expand = function
     | One a -> [a]
     | Many (cnt, elem) -> dup cnt elem in
-    match xs with
-    | [] -> []
-    | a :: rest -> (dup_expand a) @ (decode rest)
+  match xs with
+  | [] -> []
+  | a :: rest -> (dup_expand a) @ (decode rest)
 ;;
 (*
-    Run-Length Encoding of a List (Direct Solution)
-    Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem "Pack consecutive duplicates of list elements into sublists", but only count them. As in problem "Modified run-length encoding", simplify the result list by replacing the singleton lists (1 X) by X.
+  Run-Length Encoding of a List (Direct Solution)
+  Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem "Pack consecutive duplicates of list elements into sublists", but only count them. As in problem "Modified run-length encoding", simplify the result list by replacing the singleton lists (1 X) by X.
 
-    # encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
-    - : string rle list =
-    [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
-    Many (4, "e")]
-*)
+  # encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+  - : string rle list =
+  [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+  Many (4, "e")]
+ *)
 (* TODO *)
 
 (*
-    Duplicate the Elements of a List
-    Duplicate the elements of a list.
+  Duplicate the Elements of a List
+  Duplicate the elements of a list.
 
-    # duplicate ["a"; "b"; "c"; "c"; "d"];;
-    - : string list = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"]
-*)
+  # duplicate ["a"; "b"; "c"; "c"; "d"];;
+  - : string list = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"]
+ *)
 let rec duplicate = function
-    | [] -> []
-    | a :: rest -> a :: a :: (duplicate rest)
+  | [] -> []
+  | a :: rest -> a :: a :: (duplicate rest)
 ;;
 
 (*
-    Replicate the Elements of a List a Given Number of Times
-    Replicate the elements of a list a given number of times.
+  Replicate the Elements of a List a Given Number of Times
+  Replicate the elements of a list a given number of times.
 
-    # replicate ["a"; "b"; "c"] 3;;
-    - : string list = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
-*)
+  # replicate ["a"; "b"; "c"] 3;;
+  - : string list = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
+ *)
 let rec replicate xs n =
-    let rec dup_n x n = if n = 0 then [] else x :: (dup_n x (n - 1)) in
+  let rec dup_n x n = if n = 0 then [] else x :: (dup_n x (n - 1)) in
+  match xs with
+  | [] -> []
+  | a :: rest -> (dup_n a n) @ (replicate rest n)
+;;
+
+(*
+  Drop Every N'th Element From a List
+  Drop every N'th element from a list.
+
+  # drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;;
+  - : string list = ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
+ *)
+let rec drop xs n =
+  let rec drop_n acc xs =
     match xs with
     | [] -> []
-    | a :: rest -> (dup_n a n) @ (replicate rest n)
-;;
-
-(*
-    Drop Every N'th Element From a List
-    Drop every N'th element from a list.
-
-    # drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;;
-    - : string list = ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
-*)
-let rec drop xs n =
-    let rec drop_n acc xs =
-        match xs with
-        | [] -> []
-        | a :: rest -> if acc mod n = 0 then drop_n (acc + 1) rest else a :: (drop_n (acc + 1) rest)
-    in drop_n 1 xs
+    | a :: rest -> if acc mod n = 0 then drop_n (acc + 1) rest else a :: (drop_n (acc + 1) rest)
+  in drop_n 1 xs
 ;;
