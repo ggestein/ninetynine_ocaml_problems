d85ba4224e6d1d373c35d35d9e0d83a1df3ab551
ggestein <ggestein@outlook.com>
2025-09-14 12:27:48 +0800

    add extract solution

--- lists.ml
+++ lists.ml
@@ -443,3 +443,20 @@ let rec permutation xs =
      | Some s -> s :: (permutation rest)
      | None -> rest
 ;;
+(*
+  Generate the Combinations of K Distinct Objects Chosen From the N Elements of a List
+  Generate the combinations of K distinct objects chosen from the N elements of a list.
+
+  In how many ways can a committee of 3 be chosen from a group of 12 people?
+  We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients).
+  For pure mathematicians, this result may be great. But we want to really generate all the possibilities in a list.
+
+  # extract 2 ["a"; "b"; "c"; "d"];;
+  - : string list list =
+  [["a"; "b"]; ["a"; "c"]; ["a"; "d"]; ["b"; "c"]; ["b"; "d"]; ["c"; "d"]]
+ *)
+let rec extract xs =
+  match xs with
+  | [] -> []
+  | h :: tail -> (List.map (fun x -> [h; x]) tail) @ (extract tail)
+;;
